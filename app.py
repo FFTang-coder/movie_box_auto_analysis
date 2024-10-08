55dd336c7613bc37b708ca286920be1dd5d89724
zhangyuming10 <zhangyuming10@maoyan.com>
2024-10-08 16:41:19 +0800

    add minimal home page

--- app.py
+++ app.py
@@ -1,39 +1,22 @@
-from flask import Flask, render_template
-import pandas as pd
-
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def index():
-#     # 读取CSV文件
-#     df = pd.read_csv('data.csv')
-#     # 将DataFrame转换为HTML表格
-#     table = df.to_html(index=False)
-#     return render_template('index.html', table=table)
-
-
-from flask import Flask, render_template, request, jsonify
-import pandas as pd
+from flask import Flask, url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
-# 假设你的数据文件是CSV格式
-df = pd.read_excel('predict_week7_new_20241005.xlsx')
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=df.to_json(orient='records'))
-
-@app.route('/data')
-def data():
-    selected_date = request.args.get('date')
-    if selected_date:
-        # 过滤数据
-        filtered_df = df[df['预测日期'] == selected_date]
-        return jsonify(filtered_df.to_dict(orient='records'))
-    else:
-        return jsonify(df.to_dict(orient='records'))
+    return '<h1>Hello World!</h1> Welcome to my world!<img src="http://helloflask.com/totoro.gif">'
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/user/<name>')
+def user(name):
+    return f'Hello, {escape(name)}!'
+
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('index'))
+    print(url_for('user', name='germey'))
+    print(url_for('test_url_for', num=2))
+    return 'Test page'
